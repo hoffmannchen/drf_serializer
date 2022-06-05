4745415ad9a33b0d84a497016203517758dd4485
Hoffmann Chen <hoffmannchen@gmail.com>
2022-06-05 13:32:56 +0800

    0605 P908 Done

--- app01/ser.py
+++ app01/ser.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
 class BookSerializer(serializers.Serializer):
-    id = serializers.CharField()
-    title = serializers.CharField()
+    # id = serializers.CharField()
+    title = serializers.CharField(max_length=16, min_length=4)
     author = serializers.CharField()
     price = serializers.CharField()
     publisher = serializers.CharField()
+
+    def validate_price(self, data):
+        if float(data) > 10:
+            return data
+        else:
+            raise ValidationError('价格太低')
+
+    def validate(self, validate_data):
+        author = validate_data.get('author')
+        publisher = validate_data.get('publisher')
+        if publisher==author:
+            raise ValidationError('作者和出版社不能为同一人')
+        return validate_data
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title')
+        instance.price = validated_data.get('price')
+        instance.author = validated_data.get('author')
+        instance.publisher = validated_data.get('publisher')
+        instance.save()
+        return instance
--- app01/views.py
+++ app01/views.py
@@ -4,8 +4,24 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from app01.models import Book
 from app01.ser import BookSerializer
+from rest_framework.response import Response
 
 
 class BookView(APIView):
     def get(self, request, pk):
         book = Book.objects.filter(pk=pk).first()
+        book_ser = BookSerializer(book)
+        return Response(book_ser.data)
+
+    def put(self, request, pk):
+        response_msg = {'status': 100, 'msg': '成功'}
+        book = Book.objects.filter(id=pk).first()
+        book_ser = BookSerializer(instance=book, data=request.data)
+        if book_ser.is_valid():
+            book_ser.save()
+            response_msg['data'] = book_ser.data
+        else:
+            response_msg['status'] = 101
+            response_msg['msg'] = '数据校验失败'
+            response_msg['data'] = book_ser.errors
+        return Response(response_msg)
